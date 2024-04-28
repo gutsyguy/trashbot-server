b4219d42216505896f7a3435c23254cc63863ac2
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2024-04-27 17:19:26 -0700

    e

--- aws.py
+++ aws.py
@@ -1,27 +1,31 @@
 import cv2
-import boto3
+import subprocess
 import logging
 
+# Set up logging
 logging.basicConfig(level=logging.INFO)
 
-cap = cv2.VideoCapture(0)  
+# Initialize the video capture with OpenCV
+cap = cv2.VideoCapture(0)
 if not cap.isOpened():
     logging.error("Cannot open camera")
     exit()
 
+# Set video properties
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-cap.set(cv2.CAP_PROP_FPS, 20)
+cap.set(cv2.CAP_PROP_FPS, 25)
 
-kinesis_client = boto3.client('kinesisvideo')
+# Define the FFmpeg command for streaming to Amazon IVS
+rtmp_url = 'rtmp://<your-ivs-ingest-endpoint>:1935/<stream-key>'
+ffmpeg_command = ['ffmpeg', '-y', '-f', 'rawvideo', '-vcodec', 'rawvideo', '-pix_fmt', 'bgr24', 
+                  '-s', '{}x{}'.format(640, 480), '-r', '25', '-i', '-', '-c:v', 'libx264', 
+                  '-pix_fmt', 'yuv420p', '-preset', 'ultrafast', '-f', 'flv', rtmp_url]
 
-endpoint = kinesis_client.get_data_endpoint(
-    StreamName='YourKinesisVideoStream',
-    APIName='PUT_MEDIA'
-)['DataEndpoint']
+# Open pipe to FFmpeg
+process = subprocess.Popen(ffmpeg_command, stdin=subprocess.PIPE)
 
-kvs_client = boto3.client('kinesis-video-media', endpoint_url=endpoint)
-
+# Stream video frames to FFmpeg
 try:
     while True:
         ret, frame = cap.read()
@@ -29,16 +33,13 @@ try:
             logging.error("Can't receive frame (stream end?). Exiting ...")
             break
 
-        _, buffer = cv2.imencode('.jpg', frame)
-
-        kvs_client.put_media(
-            StreamName='YourKinesisVideoStream',
-            Payload=buffer.tobytes(),
-            FragmentTimecodeType='RELATIVE'
-        )
+        # Write the frame to the FFmpeg process
+        process.stdin.write(frame.tobytes())
 
 finally:
     cap.release()
+    process.stdin.close()
+    process.wait()
     cv2.destroyAllWindows()
     logging.info("Camera stream closed")
 
