3a829c33b17740a0b486d42a4cf175ab6695fcb9
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2024-04-27 18:41:36 -0700

    E

--- aws.py
+++ aws.py
@@ -6,30 +6,27 @@ from dotenv import load_dotenv
 
 load_dotenv()
 stream_key = os.environ["stream_key"]
-# Set up logging
 logging.basicConfig(level=logging.INFO)
 
-# Initialize the video capture with OpenCV
 cap = cv2.VideoCapture(0)
 if not cap.isOpened():
     logging.error("Cannot open camera")
     exit()
 
-# Set video properties
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 cap.set(cv2.CAP_PROP_FPS, 25)
 
-# Define the FFmpeg command for streaming to Amazon IVS
 rtmp_url = stream_key
 ffmpeg_command = ['ffmpeg', '-y', '-f', 'rawvideo', '-vcodec', 'rawvideo', '-pix_fmt', 'bgr24', 
                   '-s', '{}x{}'.format(640, 480), '-r', '25', '-i', '-', '-c:v', 'libx264', 
                   '-pix_fmt', 'yuv420p', '-preset', 'ultrafast', '-f', 'flv', rtmp_url]
+print("____________________________-")
+print(ffmpeg_command)
+print("____________________________-")
 
-# Open pipe to FFmpeg
 process = subprocess.Popen(ffmpeg_command, stdin=subprocess.PIPE)
 
-# Stream video frames to FFmpeg
 try:
     while True:
         ret, frame = cap.read()
@@ -37,7 +34,6 @@ try:
             logging.error("Can't receive frame (stream end?). Exiting ...")
             break
 
-        # Write the frame to the FFmpeg process
         process.stdin.write(frame.tobytes())
 
 finally:
